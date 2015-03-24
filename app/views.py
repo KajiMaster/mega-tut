73dd97b1f35e79e85f3d0d7092047d7dff2cea41
Blake McNeal <blake.mcneal@gmail.com>
2015-03-23 17:28:54 -0700

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'} # fake user
+    posts = [ # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'nickname': 'Jester'},
+            'body': "San Francisco kicks Portland's ass!"
+        }
+
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
